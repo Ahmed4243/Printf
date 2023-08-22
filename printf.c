794d214e06ece4cd254810e2e1b7bfb8b9d9ab64
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2023-08-21 19:06:05 -0700

    Idk

--- printf.c
+++ printf.c
@@ -15,6 +15,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (count = 0, len = 0; format[count] != '\0'; count++)
 	{
 		if (format[count] == '%' && format[count + 1] == 'c')
